535644451a299acdfafe55a6c9715e94770b7801
Taiki <tnsakai@gmail.com>
2022-05-18 09:51:12 -0700

    updating to git for first time in a while

--- DriftWatch_Hourly.R
+++ DriftWatch_Hourly.R
@@ -11,14 +11,17 @@ source('DriftWatchFunctions.R')
 # Destination folder
 gdriveDest <- '~/DriftWatch/'
 
-cat('------------Script version', thisVersion(), 'run on', as.character(Sys.time()), '---------------\n')
-cat('Directory set to', script.dir, '\n')
+cat('\n------------Script version', thisVersion(), 'run on', as.character(Sys.time()), '---------------')
+cat('\nDirectory set to', script.dir)
 db <- 'SPOTGPS_Logger.sqlite3'
 spotId <- '09m8vfKzAyrx3j1sSqVMCDamuAJKln1ys'
-cat('Updating GPS from API...\n')
-update <- addAPIToDb(id=spotId, db=db)
+cat('\nUpdating GPS from SPOT API...')
+updateSpot <- addAPIToDb(spotId, db=db, source='spot')
+lonestarKey <- 'f22e32e6ba5953978f0875c86f07c5ffae24b2bc'
+cat('\nUpdating GPS from Lonestar API...')
+updateLs <- addAPIToDb(lonestarKey, db=db, source='lonestar')
 # Upload to gdrive
-cat('Uploading DB to gdrive...\n')
+cat('\nUploading DB to gdrive...')
 with_drive_quiet({
     drive_auth(email='taiki.sakai@noaa.gov')
     drive_upload(db, path=gdriveDest, overwrite = TRUE)
@@ -26,25 +29,30 @@ with_drive_quiet({
 
 # current 2=RTOFSNOWCAST, 4=HYCOM
 useCurrent <- 4
-cat('Making individual drift plots...\n')
-doDriftPlots(db, verbose=T, current=useCurrent)
+cat('\nMaking individual drift plots...')
+driftPlots <- doDriftPlots(db, verbose=T, current=useCurrent)
 
-cat('Making last 2 weeks plot...\n')
+cat('\nMaking last 2 weeks plot...')
+noPlot <- c('ADRIFT_004', 'ADRIFT_008')
 recentDrifts <- getDbDeployment(db, days=14)
-plotAPIDrift(recentDrifts, filename='Last14Days.png', current=FALSE)
+recentDrifts <- recentDrifts[!(recentDrifts$DriftName %in% noPlot), ]
+twoWeekPlot <- plotAPIDrift(recentDrifts, filename='Last14Days.png', current=FALSE)
 
-cat('Making test deployment worksheet plots...\n')
-plotTestDeployments(current=useCurrent)
+cat('\nMaking test deployment worksheet plots...')
+testDepPlots <- plotTestDeployments(current=useCurrent)
 
-cat('Uploading plots to drive...\n')
-with_drive_quiet({
-    allPngs <- list.files(pattern='png$', full.names = TRUE, recursive = FALSE)
-    for(i in seq_along(allPngs)) {
-        modtime <- file.info(allPngs[i])$mtime
-        diff <- as.numeric(difftime(Sys.time(), modtime, units='hours'))
-        if(diff > 1) next
-        drive_upload(allPngs[i], path=gdriveDest, overwrite = TRUE)
-    }
-})
+cat('\nUploading plots to drive...')
+doGdriveUpload(c(driftPlots, twoWeekPlot), paste0(gdriveDest, 'DriftPlots/'))
 
-cat('All completed successfully!')
+doGdriveUpload(testDepPlots, paste0(gdriveDest, 'TestDeploymentPlots/'))
+# with_drive_quiet({
+#     allPngs <- list.files(pattern='png$', full.names = TRUE, recursive = FALSE)
+#     for(i in seq_along(allPngs)) {
+#         modtime <- file.info(allPngs[i])$mtime
+#         diff <- as.numeric(difftime(Sys.time(), modtime, units='hours'))
+#         if(diff > 1) next
+#         drive_upload(allPngs[i], path=gdriveDest, overwrite = TRUE)
+#     }
+# })
+
+cat('\nAll completed successfully!')
